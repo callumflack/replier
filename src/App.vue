0048cd3f616b409d871527ace3b0db1f4156ca3a
Barry Hood <barryph@protonmail.com>
2019-09-07 04:40:38 +1200

    Add page transition

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,29 @@
     <div id="nav" class="container Block-sm max-w-5xl">
       <router-link to="/">Corvid Writing</router-link>
     </div>
-    <router-view/>
+    <vue-page-transition :name="transitionName">
+      <router-view/>
+    </vue-page-transition>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      // Default for home page which doesn't trigger $route watcher
+      transitionName: 'fade-in-right',
+    };
+  },
+  watch: {
+    $route(to, from) {
+      const toDepth = to.path.split('/').filter(s => s.length).length;
+      const fromDepth = from.path.split('/').filter(s => s.length).length;
+      this.transitionName = toDepth < fromDepth ? 'fade-in-right' : 'fade-in-left';
+    },
+  },
+};
+</script>
 
 <style>
 #nav a.router-link-active {
