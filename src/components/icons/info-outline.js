d4a07c191df910d35334f2ab34671ff74584e609
Callum Flack <callumflack@gmail.com>
2020-04-27 20:34:35 +1000

    use outline info icon to complement new Logo

---
+++ src/components/icons/info-outline.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+var icon = require('vue-svgicon')
+icon.register({
+  'info-outline': {
+    width: 16,
+    height: 16,
+    viewBox: '0 0 24 24',
+    data: '<path pid="0" d="M0 0h24v24H0z" fill="none"/><path pid="1" d="M12 2a10 10 0 1010 10A10 10 0 0012 2zm0 19a9 9 0 119-9 9 9 0 01-9 9zm-1-4h2v-6h-2zm0-8h2V7h-2z"/>'
+  }
+})
