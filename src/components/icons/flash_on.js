3034e7ad211c39bd5238240642f3f53622d9db9d
Callum Flack <callumflack@gmail.com>
2020-05-18 21:57:01 +1000

    notif icons

---
+++ src/components/icons/flash_on.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+var icon = require('vue-svgicon')
+icon.register({
+  'flash_on': {
+    width: 24,
+    height: 24,
+    viewBox: '0 0 24 24',
+    data: '<path pid="0" d="M0 0h24v24H0z" fill="none"/><path pid="1" d="M7 2v11h3v9l7-12h-4l4-8z"/>'
+  }
+})
